72cc4ef5d8b5dd88a59bee87532071adf9cd6485
NeXuZSakura <120510043+NeXuZSakura@users.noreply.github.com>
2024-06-25 18:50:54 +0300

    Screens beta

--- app/src/main/java/com/example/pipesdetector/MainActivity.kt
+++ app/src/main/java/com/example/pipesdetector/MainActivity.kt
@@ -13,15 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userData : EditText = findViewById(R.id.UserNameLogInScreen)
-        val userPassword : EditText = findViewById(R.id.UserPasswordLogInScreen)
-        val logInButton : Button = findViewById(R.id.LogInButton)
-        val toRegistrationButton : Button = findViewById(R.id.ToRegInScreenButton)
+        val userLogin : EditText = findViewById(R.id.UserNameLogInScreen)  // Логин для входа
+        val userPassword : EditText = findViewById(R.id.UserPasswordLogInScreen) // Пароль для входа
+        val logInButton : Button = findViewById(R.id.LogInButton) // Кнопка входа
+        val toRegistrationButton : Button = findViewById(R.id.ToRegInScreenButton) // Кнопка перехода в окно регистрации
 
         logInButton.setOnClickListener {
-
+            // Миша или Родион добавьте сюда проверку через ваш клиент-сервер
+            // что такой пользователь существует
         }
 
+        // Реализация перехода в окно регистрации
         toRegistrationButton.setOnClickListener {
             val intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
