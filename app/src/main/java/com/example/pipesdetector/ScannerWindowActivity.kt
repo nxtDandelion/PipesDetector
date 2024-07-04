cd7091c92350c596c33a73fd84bb7e196088a6aa
NeXuZSakura <120510043+nxtDandelion@users.noreply.github.com>
2024-07-04 19:20:58 +0300

    Add scanning pipes WITHOUT AI v0.8

--- app/src/main/java/com/example/pipesdetector/ScannerWindowActivity.kt
+++ app/src/main/java/com/example/pipesdetector/ScannerWindowActivity.kt
@@ -1,20 +1,46 @@
 package com.example.pipesdetector
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+import android.util.Base64
+import android.widget.TextView
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.IOException
+
 
 class ScannerWindowActivity : AppCompatActivity() {
     private val CAMERA_REQUEST_CODE = 1
     private val CAMERA_PERMISSION_CODE = 2
     private val GALLERY_PERMISSION_CODE = 3
     private val GALLERY_REQUEST_CODE = 4
+    lateinit var firstImageID: ImageView
+    lateinit var secondImageID: ImageView
+    lateinit var countOfPipesText : TextView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scanner_window)
@@ -22,31 +48,47 @@ class ScannerWindowActivity : AppCompatActivity() {
         getPermissions()
 
         val cameraButton: Button = findViewById(R.id.ButtonForCamera)
-        val galleryButton : Button = findViewById(R.id.ButtonForGalery)
+        val galleryButton: Button = findViewById(R.id.ButtonForGalery)
+        countOfPipesText = findViewById(R.id.NumberOfPipes)
+        firstImageID = findViewById(R.id.imageBeforeNeuro)
+        secondImageID = findViewById(R.id.imageAfterNeuro)
 
-        cameraButton.setOnClickListener(){
+        cameraButton.setOnClickListener() {
             val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
         }
 
-        galleryButton.setOnClickListener(){
+        galleryButton.setOnClickListener() {
             val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
         }
     }
 
-    private fun getPermissions() : Boolean{
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
-            ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+    private fun getPermissions(): Boolean {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
         ) {
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                arrayOf(
+                    android.Manifest.permission.CAMERA,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
                 CAMERA_PERMISSION_CODE
             )
             return false
         }
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
@@ -58,12 +100,89 @@ class ScannerWindowActivity : AppCompatActivity() {
     }
 
     @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode:Int, resultCode:Int, data:Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            // фото с камеры
+            val photo = data?.extras?.get("data") as Bitmap
+            firstImageID.setImageBitmap(photo)
+            val imageString = encodeImageToString(photo)
+            GlobalScope.launch {
+                val (imageFromBackend, pipesCount) = sendRequestToBackend(imageString)
+                val imageBitmapAfterDecode = decodeStringToImage(imageFromBackend)
+                runOnUiThread {
+                    secondImageID.setImageBitmap(imageBitmapAfterDecode)
+                    countOfPipesText.setText("Число труб на фото: $pipesCount")
+                }
+            }
         } else if (requestCode == GALLERY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            // изображения из галереи
+            val selectedImageUri = data?.data
+            val inputStream = selectedImageUri?.let { contentResolver.openInputStream(it) }
+            val imageBitmap = BitmapFactory.decodeStream(inputStream)
+            firstImageID.setImageBitmap(imageBitmap)
+            val imageString = encodeImageToString(imageBitmap)
+            GlobalScope.launch {
+                val (imageFromBackend, pipesCount) = sendRequestToBackend(imageString)
+                val imageBitmapAfterDecode = decodeStringToImage(imageFromBackend)
+                runOnUiThread {
+                    secondImageID.setImageBitmap(imageBitmapAfterDecode)
+                    countOfPipesText.setText("Число труб на фото: $pipesCount")
+                }
+            }
+        }
+    }
+
+    private fun encodeImageToString(imageBitmap: Bitmap): String {
+        val baos = ByteArrayOutputStream()
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val imageBytes = baos.toByteArray()
+        return Base64.encodeToString(imageBytes, Base64.DEFAULT)
+    }
+
+    private fun decodeStringToImage(imageString: String): Bitmap {
+        val imageBytes = Base64.decode(imageString, Base64.DEFAULT)
+        val stream = ByteArrayInputStream(imageBytes)
+        return BitmapFactory.decodeStream(stream)
+    }
+
+    private suspend fun sendRequestToBackend(imageString: String) : Pair<String, Int> {
+        return withContext(Dispatchers.IO) {
+            var encodedImage = ""
+            var objectsCount = 0
+
+            val jsonObject = JSONObject()
+            jsonObject.put("imgString", imageString)
+
+            val jsonObjectString = jsonObject.toString()
+            val url = URL("http://192.168.0.177:8080/countPipes")
+
+            (url.openConnection() as? HttpURLConnection)?.run {
+                requestMethod = "POST"
+                setRequestProperty("Content-Type", "application/json")
+                setRequestProperty("Accept", "application/json")
+                doOutput = true
+
+                try {
+                    outputStream.use { it.write(jsonObjectString.toByteArray()) }
+
+                    val responseCode = responseCode
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        inputStream.bufferedReader().use { reader ->
+                            val jsonResponse = JSONObject(reader.readText())
+                            encodedImage = jsonResponse.getString("imgString")
+                            objectsCount = jsonResponse.getInt("quantityPipes")
+                        }
+                    } else {
+                        Log.e("Error", "Failed to send request. Response code: $responseCode")
+                    }
+                } catch (e: IOException) {
+                    Log.e("Error", "Failed to send request: ${e.message}")
+                } finally {
+                    disconnect()
+                }
+            } ?: run {
+                Log.e("Error", "Connection not established")
+            }
+            Pair(encodedImage, objectsCount)
         }
     }
 }
